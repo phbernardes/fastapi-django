e990756b3e7c89d3d2d521145140e42bb848ab00
pedro-prose <pedro.bernardes@prosehair.com>
2022-05-15 18:49:27 +0200

    Define user model

--- fastdjango/settings.py
+++ fastdjango/settings.py
@@ -124,3 +124,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+USER_MODEL = "users.User"
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    pubkey = models.UUIDField(
+        default=uuid.uuid4, db_index=True, editable=False, unique=True
+    )
+
+    EMAIL_FIELD = "username"
+    USERNAME_FIELD = "username"
+
+    def __str__(self):
+        return f"{self.username} - {self.first_name} {self.last_name}"
