c65823350be0de61f98e087534a3f11b3820efc0
pedro-prose <pedro.bernardes@prosehair.com>
2022-05-15 15:12:23 +0200

    Reformat code base with black

--- fastdjango/wsgi.py
+++ fastdjango/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fastdjango.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fastdjango.settings")
 
 application = get_wsgi_application()
